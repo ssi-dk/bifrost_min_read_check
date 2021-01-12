ad8ffdc48bc1cc0707e2d98336bc359ea433b83e
Martín Basterrechea <martinbaste@users.noreply.github.com>
2021-01-12 10:50:15 +0100

    Update rule__greater_than_min_reads_check.py

--- bifrost_min_read_check/rule__greater_than_min_reads_check.py
+++ bifrost_min_read_check/rule__greater_than_min_reads_check.py
@@ -6,8 +6,8 @@ from bifrostlib import common
 def rule__greater_than_min_reads_check(input: object, output: object, component_json: Dict, log: object) -> None:
     try:
         num_of_reads: int = int(common.get_group_from_file("Result:\s*([0-9]+)\sreads", input.stats_file))
-        min_read_number:int = component_json["options"]["min_num_reads"]
-        has_min_num_of_reads:bool = False
+        min_read_number: int = component_json["options"]["min_num_reads"]
+        has_min_num_of_reads: bool = False
         if num_of_reads > min_read_number:
             has_min_num_of_reads = True
         with open(output._file, "w") as fh:
